35afa550f487f3b174c1b72f2e2c8200889b02bf
AbnerStelmastchuk <stelmastchukabner@gmail.com>
2021-11-04 17:02:31 -0300

    Cadastro de conta && Validando CPF existente!

--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/courses', (request, response) => {
-  const query = request.query
-  console.log(query)
-  return response.json(['Curso 1', 'Curso 2', 'Curso 3'])
+const customers = []
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body
+  const id = uuidv4()
+
+  const customerAlreadyExists = customers.some(
+    customers => customers.cpf === cpf
+  )
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer already exists!' })
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  })
+  return response.status(201).send()
 })
 
-app.post('/courses', (request, response) => {
-  const body = request.body
-  console.log(body)
-  return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4'])
-})
-
-app.put('/courses/:id', (request, response) => {
-  const { id } = request.params
-  console.log(id)
-  return response.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4'])
-})
-
-app.patch('/courses/:id', (request, response) => {
-  return response.json(['Curso 6', 'Curso 7', 'Curso 3', 'Curso 4'])
-})
-
-app.delete('/courses/:id', (request, response) => {
-  return response.json(['Curso 6', 'Curso 7', 'Curso 4'])
-})
 app.listen(3333)
